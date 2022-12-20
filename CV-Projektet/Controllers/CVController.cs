e662cd83303d367a53b60e376a3f522074821689
LisaKjellgren <lisakjellgren@hotmail.com>
2022-12-20 15:55:50 +0100

    Hjälp

--- CV-Projektet/Controllers/CVController.cs
+++ CV-Projektet/Controllers/CVController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CV_Projektet.Data;
+using CV_Projektet.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CV_Projektet.Controllers
 {
-	public class CVController : Controller
+	public class CVController : Controller	
 	{
+		private ApplicationDbContext context;
+		public CVController(ApplicationDbContext cntx)
+		{
+			context = cntx;
+		}
+
+		[HttpGet]
 		public IActionResult Index()
 		{
-			return View();
+			List<CV> CVlist = context.CVs.ToList();
+			return View(CVlist);
 		}
 	}
 }
