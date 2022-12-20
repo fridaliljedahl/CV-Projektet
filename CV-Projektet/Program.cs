ac020d8c271497e6dbcb55123427fdf5cc68036a
LisaKjellgren <lisakjellgren@hotmail.com>
2022-12-20 15:13:00 +0100

    Fix med CV

--- CV-Projektet/Program.cs
+++ CV-Projektet/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
