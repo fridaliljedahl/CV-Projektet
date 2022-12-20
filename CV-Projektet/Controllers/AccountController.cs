b05baf2db9ed76e0ad7d53f80fa0a799948dd9d9
viktorhg <96234553+viktorhg@users.noreply.github.com>
2022-12-20 14:04:42 +0100

    Logingrejer

---
+++ CV-Projektet/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using CV_Projektet.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Projektet.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            LoginViewModel logInViewModel = new LoginViewModel();
+            return View(logInViewModel);
+        }
+    }
+}
