7090a8955d7a2ca22f75cba1e4bc318ffdc9b155
fridaliljedahl <fridaliljedahl@hotmail.com>
2022-12-27 15:10:16 +0100

    man kan nu även gå med/gå ur projekt från sidan med alla projekt

--- CV-Projektet/Controllers/UserController.cs
+++ CV-Projektet/Controllers/UserController.cs
@@ -18,22 +18,37 @@ namespace CV_Projektet.Controllers
             this.context = context;
         }
 
-        public ActionResult JoinProject(int projectId)
-        { 
+        public ActionResult JoinProject(int projectId, string caller)
+        {
             User_Projects up = new User_Projects();
             up.ProjectID = projectId;
             up.UserID = userManager.GetUserId(User);
             context.User_Projects.Add(up);
             context.SaveChanges();
-            return RedirectToAction("Index", "Home"); 
+            if (caller.Equals("Home"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Projects");
+            }
+
         }
-        public ActionResult LeaveProject(int projectId)
+        public ActionResult LeaveProject(int projectId, string caller)
         {
             var up = context.User_Projects.Where
                 (p => p.ProjectID == projectId && p.UserID == userManager.GetUserId(User)).Single();
             context.User_Projects.Remove(up);
             context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            if (caller.Equals("Home"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Projects");
+            }
         }
     }
 }
